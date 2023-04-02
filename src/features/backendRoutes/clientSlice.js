b03f2c42b12aa9f295fc26cd63650beae72c5ea7
collskin <115164621+collskin@users.noreply.github.com>
2023-04-02 20:25:50 +0200

    Added token authorization with login and signup validation

--- src/features/backendRoutes/clientSlice.js
+++ src/features/backendRoutes/clientSlice.js
@@ -1,46 +1,69 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 import { baseUrl } from "../axios";
-
+import Cookies from "universal-cookie";
 
 const initialState = {
-    currentClient: {},
-    isLoading: false,
-  };
+  currentClient: {},
+  isLoading: false,
+};
+const cookies = new Cookies();
 
-
-  export const createClient = createAsyncThunk(
-    "client/createClient",
-  
-    async (client, thunkAPI) => {
-      try {
-        const resp = await baseUrl.post("client/", client, thunkAPI);
-        console.log(resp);
-        return resp.data;
-        
-      
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error);
-      }
+export const createClient = createAsyncThunk(
+  "client/createClient",
+  async (client, thunkAPI) => {
+    try {
+      const resp = await baseUrl.post("client/sign-up/", client, thunkAPI);
+      cookies.set("token", resp.data.token, { path: "/" });
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
-  );
+  }
+);
 
-  const clientSlice = createSlice({
-    name:"client",
-    initialState,
-    extraReducers:{
-      [createClient.fulfilled]: (state, { payload }) => {
-        state.isLoading = false;
-        state.currentClient = payload;
-        toast.success("Successfully created account");
-      },
-      [createClient.pending]: (state) => {
-        state.isLoading = true;
-      },
-      [createClient.rejected]: (state, { payload }) => {
-        state.isLoading = true;
-        toast.error(payload.response.data.detail);
-      },
-  }});
-  
-  export default clientSlice.reducer;
+export const loginClient = createAsyncThunk(
+  "client/loginClient",
+  async (client, thunkAPI) => {
+    try {
+      const resp = await baseUrl.post("client/sign-in/", client, thunkAPI);
+      cookies.set("token", resp.data.token, { path: "/" });
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+const clientSlice = createSlice({
+  name: "client",
+  initialState,
+  extraReducers: {
+    [createClient.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.currentClient = payload;
+      toast.success("Successfully created account!");
+    },
+    [createClient.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [createClient.rejected]: (state, { payload }) => {
+      state.isLoading = true;
+      toast.error(payload.response.data.detail);
+    },
+    [loginClient.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.currentClient = payload;
+      toast.success("Successfully logged in!");
+    },
+    [loginClient.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [loginClient.rejected]: (state, { payload }) => {
+      state.isLoading = true;
+      toast.error(payload.response.data.detail);
+    },
+  },
+});
+
+export default clientSlice.reducer;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,137 +1,196 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signup.css";
-import { IoIosArrowBack } from "react-icons/io";
 import Topbar from "../../components/topbar/Topbar";
-import TelegramIcon from "../../assets/img/telegram-icon.png";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { createClient } from "../../features/backendRoutes/clientSlice";
+import {
+  createClient,
+  loginClient,
+} from "../../features/backendRoutes/clientSlice";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { toast } from "react-toastify";
-
 
 export default function SignUp() {
-
   const dispatch = useDispatch();
+  const [active, setActive] = useState(false);
+
   const formik = useFormik({
     initialValues: {
       first_name: "",
       last_name: "",
       phone_number: "",
       email: "",
+      password: "",
     },
     validationSchema: Yup.object({
       first_name: Yup.string().required("Required"),
       last_name: Yup.string().required("Required"),
-      phone_number: Yup.string().required("Required"),
-      email: Yup.string().email("Invalid email address"),
+      phone_number: Yup.string()
+        .matches(/^\d+$/, "You can only enter numbers")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(6, "Password must be at least 6 characters")
+        .matches(/^(?=.*\d).+$/, "Password must contain at least one number")
+        .required("Required"),
     }),
-    onSubmit: async (values) => {
-      try {
-        const resp = await dispatch(createClient(values));
-        if (resp.meta.requestStatus === "fulfilled") {
-          navigate("/");
-        }
-      } catch (error) {
-        console.log(error);
-      }
+    onSubmit: (values, { resetForm }) => {
+      values.phone_number = parseInt(values.phone_number);
+      dispatch(createClient(JSON.stringify(values, null, 2)));
+      resetForm();
     },
   });
 
-  const navigate = useNavigate();
-  const handleBack = () => {
-    navigate(-1);
-  };
-  const handleTelegram = () => {
-    window.open("https://telegram.org/", "_blank");
-  };
+  const loginValidation = useFormik({
+    initialValues: {
+      phone_number: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      phone_number: Yup.string()
+        .matches(/^\d+$/, "You can only enter numbers")
+        .required("Required"),
+      password: Yup.string()
+        .min(6, "Password must be at least 6 characters")
+        .matches(/^(?=.*\d).+$/, "Password must contain at least one number")
+        .required("Required"),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      values.phone_number = parseInt(values.phone_number);
+      dispatch(loginClient(JSON.stringify(values, null, 2)));
+      resetForm();
+    },
+  });
 
   return (
-    <div className="sign-up-main-div">
+    <>
       <Topbar />
-
-      <div className="sign-up-header">
-        <IoIosArrowBack className="sign-up-back-icon" onClick={handleBack} />
-        <span className="sign-up-back-span" onClick={handleBack}>
-          Back
-        </span>
-        <h3 className="sign-up-h3">Sign up</h3>
-      </div>
-      <div className="sign-up-wrapper">
-        <form onSubmit={formik.handleSubmit}>
-          <div className="sign-up-left">
-            <span className="sign-up-span">Phone number</span>
-            <input
-              type="text"
-              name="phone_number"
-              className="sign-up-phone-input"
-              onBlur={formik.handleBlur}
-              value={formik.values.phone_number}
-              onChange={formik.handleChange}
-            />
-            {formik.touched.phone_number && formik.errors.phone_number ? (
-              <p className="sign-up-err">{formik.errors.phone_number}</p>
-            ) : null}
-            <span className="sign-up-span">Name</span>
-            <input
-              type="text"
-              name="first_name"
-              className="sign-up-name-input"
-              onBlur={formik.handleBlur}
-              value={formik.values.first_name}
-              onChange={formik.handleChange}
-            />
-            {formik.touched.first_name && formik.errors.first_name ? (
-              <p className="sign-up-err">{formik.errors.first_name}</p>
-            ) : null}
-            <span className="sign-up-span">Surname</span>
-            <input
-              type="text"
-              name="last_name"
-              className="sign-up-surname-input"
-              onBlur={formik.handleBlur}
-              value={formik.values.last_name}
-              onChange={formik.handleChange}
-            />
-            {formik.touched.last_name && formik.errors.last_name ? (
-              <p className="sign-up-err">{formik.errors.last_name}</p>
-            ) : null}
-
-            <span className="sign-up-span">Email*</span>
-            <input
-              type="email"
-              name="email"
-              className="sign-up-email-input"
-              onBlur={formik.handleBlur}
-              value={formik.values.email}
-              onChange={formik.handleChange}
-            />
-
-            {formik.touched.email && formik.errors.email ? (
-              <p className="sign-up-err">{formik.errors.email}</p>
-            ) : null}
-
-            <button className="sign-up-btn" type="submit">
-              SIGN UP
-            </button>
+      <div className="login-container">
+        <div className={active ? "container" : "container right-panel-active"}>
+          <div className="form-container sign-up-container">
+            <form action="none" onSubmit={formik.handleSubmit}>
+              <h1 className="sign-in-h2">Sign up</h1>
+              <span className="spanHome">Phone number</span>
+              <input
+                type="text"
+                name="phone_number"
+                onBlur={formik.handleBlur}
+                value={formik.values.phone_number}
+                onChange={formik.handleChange}
+              />
+              {formik.touched.phone_number && formik.errors.phone_number ? (
+                <p className="paragraph-error">{formik.errors.phone_number}</p>
+              ) : null}
+              <span className="spanHome">Name</span>
+              <input
+                type="text"
+                name="first_name"
+                onBlur={formik.handleBlur}
+                value={formik.values.first_name}
+                onChange={formik.handleChange}
+              />
+              {formik.touched.first_name && formik.errors.first_name ? (
+                <p className="paragraph-error">{formik.errors.first_name}</p>
+              ) : null}
+              <span className="spanHome"> Surname</span>
+              <input
+                type="text"
+                name="last_name"
+                onBlur={formik.handleBlur}
+                value={formik.values.last_name}
+                onChange={formik.handleChange}
+              />
+              {formik.touched.last_name && formik.errors.last_name ? (
+                <p className="paragraph-error">{formik.errors.last_name}</p>
+              ) : null}
+              <span className="spanHome">email</span>
+              <input
+                type="email"
+                name="email"
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <p className="paragraph-error">{formik.errors.email}</p>
+              ) : null}
+              <span className="spanHome">Password</span>
+              <input
+                type="password"
+                name="password"
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <p className="paragraph-error">{formik.errors.password}</p>
+              ) : null}
+              <button className="btnHome" type="submit">
+                Sign Up
+              </button>
+            </form>
           </div>
-        </form>
-        <div className="sign-up-right">
-          <h4 className="sign-up-h4">Description</h4>
-          <p className="sign-up-paragraph">
-            Блок доп.информации об объекте Блок доп.информации об объекте Блок
-            доп.информации об объекте Блок доп.информации об объекте Блок
-            доп.информации об объекте Блок доп.информации об объекте
-          </p>
-          <span className="sign-up-span-messanger">Supported messangers</span>
-          <img
-            src={TelegramIcon}
-            className="sign-up-telegram-icon"
-            onClick={handleTelegram}
-          />
+          <div className="form-container sign-in-container">
+            <form action="none" onSubmit={loginValidation.handleSubmit}>
+              <h1 className="sign-in-h2">Sign in</h1>
+              <span className="spanHome">Phone number</span>
+              <input
+                type="text"
+                name="phone_number"
+                onBlur={loginValidation.handleBlur}
+                value={loginValidation.values.phone_number}
+                onChange={loginValidation.handleChange}
+              />
+              {loginValidation.touched.phone_number &&
+              loginValidation.errors.phone_number ? (
+                <p className="paragraph-error">
+                  {loginValidation.errors.phone_number}
+                </p>
+              ) : null}
+              <span className="spanHome">Password</span>
+              <input
+                type="password"
+                name="password"
+                onBlur={loginValidation.handleBlur}
+                value={loginValidation.values.password}
+                onChange={loginValidation.handleChange}
+              />
+              {loginValidation.touched.password &&
+              loginValidation.errors.password ? (
+                <p className="paragraph-error">
+                  {loginValidation.errors.password}
+                </p>
+              ) : null}
+              <button className="btnHome" type="submit">
+                Sign In
+              </button>
+            </form>
+          </div>
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-left">
+                <h1 className="h1Home">Already registered?</h1>
+                <p>Sign in to your account to continue!</p>
+                <button
+                  className="ghost"
+                  onClick={() => setActive((prev) => !prev)}
+                >
+                  Sign In
+                </button>
+              </div>
+              <div className="overlay-panel overlay-right">
+                <h1 className="h1Home">First time here?</h1>
+                <p>You can create an account to rent and rent out spaces!</p>
+                <button
+                  className="ghost q"
+                  onClick={() => setActive((prev) => !prev)}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
